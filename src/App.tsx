db6f49462dcb89f56c48727ba613106880a135b0
Stiven <stivencastillo.90@gmail.com>
2021-11-27 11:27:20 -0500

    Init project, install ts, eslint, tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-red-500">
+      <h1>test</h1>
+    </div>
+  );
+};
+
+export default App;
